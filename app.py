5d67cc350906ce96d88ef7a7833d3c4f1c65e356
David Grigoryan <david_grigoryan@mail.ru>
2017-10-21 03:28:42 +0300

    1. Change .gitignore 2. Refactor app.py

--- app.py
+++ app.py
@@ -18,5 +18,4 @@ class User(Resource):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-    model = prediction.train()
+    app.run(debug=True, host='192.168.0.1', port=5000)
