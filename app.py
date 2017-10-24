20ec5bf7d4cf501d4b892decdc9e678509c8b766
Serob Balyan <serob.balyan@gmail.com>
2017-10-24 15:56:58 +0300

    Change app.py and encoding of prediction for real hosting

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 
 from flask import request, Response, jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 import prediction
 from init_api import api, app
 
@@ -12,33 +12,30 @@ model_sg = prediction.train(1)
 @api.route('/prediction')
 class User(Resource):
     def get(self):
-        args = request.args
-        if not dict(request.args).keys():
-            r = Response(response=str({"predicted word": []}).replace("'", "\""))
-            return r
-        else:
-            method = 'sbow'  # by default
-            if list(args.keys()).count('method') == 1:
-                method = args['method']
-            message = args['message']
-            if message == '':
-                return Response(response=str({"predict word": []}).replace("'", "\""))
-            if method == 'sg':
-                pred_messages = list(map(lambda x: x[0], prediction.predict(message, model_sg)))
-                data = str({"predicted word": pred_messages}).replace("'", "\"")
-                print(data)
-                r = Response(response=data)
-                return r
-            else:
-                pred_messages = list(map(lambda x: x[0], prediction.predict(message, model_sbow)))
-                data = str({"predicted word": pred_messages}).replace("'", "\"")
-                print(data)
-                r = Response(response=data)
-                return r
+        JSON_NAME="predict worded"
+        parser = reqparse.RequestParser()
+        parser.add_argument('message', type=str, required=True)
+        args = parser.parse_args(strict=True)
+        message = args['message']
+
+        if message == '':
+            return {JSON_NAME: []}
+
+        method = 'sbow'  # by default
+        if list(args.keys()).count('method') == 1:
+            method = args['method']
+        model_method = model_sg if method == 'sg' else model_sbow
+
+        pred_list = prediction.predict(message, model_method)
+        pred_messages = list(map(lambda x: x[0], pred_list)) if pred_list is not None else []
+        data = str({JSON_NAME: pred_messages}).replace("'", "\"")
+        print(data)
+        r = Response(response=data)
+        return r
 
     def post(self, username):
         return {'post username': username}
 
-
+# remove on uploading hosting server
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
